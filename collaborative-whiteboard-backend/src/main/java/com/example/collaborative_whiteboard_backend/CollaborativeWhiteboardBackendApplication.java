ec2b157ccba21fbeecf4dc8d19844fcc6d64fcb8
Adarsh.M <adarshmelath1305@gmail.com>
2026-04-23 01:47:25 +0530

    chore: initial scaffold

---
+++ collaborative-whiteboard-backend/src/main/java/com/example/collaborative_whiteboard_backend/CollaborativeWhiteboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.collaborative_whiteboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeWhiteboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeWhiteboardBackendApplication.class, args);
+	}
+
+}
